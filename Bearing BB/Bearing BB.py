2250ed223fcdf9a61428bbf7e12da21aea009b3e
Daniel <50288378+DAguirreAg@users.noreply.github.com>
2019-08-20 13:50:49 +0200

    add files

---
+++ Bearing BB/Bearing BB.py
@@ -0,0 +1,147 @@
+#Author-Daniel Aguirre
+#Description-A script for making custom made bearing with a 3D printer and some BB´s
+import adsk.core, adsk.fusion, traceback
+import math
+
+# Bearings variables
+
+_units = "mm"
+
+h = 10
+di = 9
+do = 30
+dbb = 6
+dbb_extract = 2
+wgap = 1
+
+
+def run(context):
+    ui = None
+    try:
+        #checkValues()
+    
+        # Convert to "cm" (because scripts works with cm)    
+        if _units == "mm": # 1mm -> 0.1cm 
+            scale = 0.1 
+        elif _units == "m": # 1m -> 100cm 
+            scale = 100
+        else:
+            scale = 1
+       
+        global h, di, do, dbb, dbb_extract, wgap
+            
+        h = h*scale
+        di = di*scale
+        do = do*scale
+        dbb = dbb*scale
+        dbb_extract = dbb_extract*scale
+        wgap = wgap*scale
+
+        
+        app = adsk.core.Application.get()
+        ui = app.userInterface        
+        doc = app.documents.add(adsk.core.DocumentTypes.FusionDesignDocumentType)
+        
+        design = app.activeProduct
+        
+        # Get the root component of the active design.
+        rootComp = design.rootComponent
+        
+        # Get extrude and revolve features
+        extrudes = rootComp.features.extrudeFeatures 
+        revolves = rootComp.features.revolveFeatures
+        
+        # Create basic construction planes.
+        sketches = rootComp.sketches
+        xyPlane = rootComp.xYConstructionPlane        
+        yzPlane = rootComp.yZConstructionPlane
+        
+        
+        
+        # STEP 1: Draw inner and outer diameters
+        sketch1 = sketches.add(xyPlane)
+        circles = sketch1.sketchCurves.sketchCircles
+        circle1 = circles.addByCenterRadius(adsk.core.Point3D.create(0, 0, 0), di/2)
+        circle2 = circles.addByCenterRadius(adsk.core.Point3D.create(0, 0, 0), do/2)
+        
+        # STEP 2: Extrude (create) previous circles   
+        prof1 = sketch1.profiles.item(1)
+        extInput1 = extrudes.createInput(prof1, adsk.fusion.FeatureOperations.NewBodyFeatureOperation)
+        extInput1.setSymmetricExtent(adsk.core.ValueInput.createByReal(h/2), False)
+        extrude1 = extrudes.add(extInput1)
+        
+        # STEP 3: Draw gap circles     
+        dm = (di + do)/2        
+        sketch2 = sketches.add(xyPlane)
+        circles = sketch2.sketchCurves.sketchCircles
+        circle3 = circles.addByCenterRadius(adsk.core.Point3D.create(0, 0, 0), (dm + wgap/2)/2)
+        circle4 = circles.addByCenterRadius(adsk.core.Point3D.create(0, 0, 0), (dm - wgap/2)/2)
+        circle5 = circles.addByCenterRadius(adsk.core.Point3D.create(0, 0, 0), dm/2)
+        circle5.isConstruction = True
+        
+        # STEP 4: Extrude (cut) with previous circles
+        prof2 = sketch2.profiles.item(0)        
+        extInput2 = extrudes.createInput(prof2, adsk.fusion.FeatureOperations.CutFeatureOperation)
+        extInput2.setSymmetricExtent(adsk.core.ValueInput.createByReal(h/2), False)
+        extrude2 = extrudes.add(extInput2)        
+        
+        # STEP 5: Draw BB circle
+        sketch3 = sketches.add(yzPlane)
+        circles = sketch3.sketchCurves.sketchCircles
+        circle6 = circles.addByCenterRadius(adsk.core.Point3D.create(0, dm/2, 0), dbb/2)
+        
+        # STEP 6: Revolute (cut) with previous circles        
+        # Draw a line to use as the axis of revolution.
+        lines = sketch3.sketchCurves.sketchLines
+        axisLine = lines.addByTwoPoints(adsk.core.Point3D.create(-h/2, 0, 0), adsk.core.Point3D.create(h/2, 0, 0))
+        axisLine.isConstruction = True
+        
+        prof3 = sketch3.profiles.item(0)        
+        revInput = revolves.createInput(prof3, axisLine, adsk.fusion.FeatureOperations.CutFeatureOperation)
+        angle = adsk.core.ValueInput.createByReal(2*math.pi)
+        revInput.setAngleExtent(False, angle) 
+        revolute1 = revolves.add(revInput)        
+        
+        # STEP 7: Draw insertion circle
+        sketch4 = sketches.add(xyPlane)
+        circles = sketch4.sketchCurves.sketchCircles  
+        circle7 = circles.addByCenterRadius(adsk.core.Point3D.create(0, dm/2, 0), dbb_extract/2)
+        circle7 = circles.addByCenterRadius(adsk.core.Point3D.create(0, dm/2, 0), dbb/2)
+        
+        # STEP 8: Extrude (cut) with previous circles
+        prof4 = sketch4.profiles.item(0)        
+        extInput3 = extrudes.createInput(prof4, adsk.fusion.FeatureOperations.CutFeatureOperation)
+        extInput3.setSymmetricExtent(adsk.core.ValueInput.createByReal(h/2), False)
+        extrude3 = extrudes.add(extInput3)
+        
+        prof5 = sketch4.profiles.item(1)        
+        extInput4 = extrudes.createInput(prof5, adsk.fusion.FeatureOperations.CutFeatureOperation)
+        extInput4.setOneSideExtent(adsk.fusion.ThroughAllExtentDefinition.create(), adsk.fusion.ExtentDirections.PositiveExtentDirection)
+        extrude4 = extrudes.add(extInput4)   
+        
+       
+        
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+            
+            
+            
+def checkInputValues():
+    
+    # Check values bigger than 0
+    ## TO DO
+    
+    # Check there is enough space for the balls
+    if ((do-di)/2 <= dbb):
+        thickness = 2
+        do = 2*(dbb + 2*thickness) + di
+        if ui:
+            ui.messageBox("Bearing's width too thin. Modified Outside diameter to: " + str(do))
+            
+    if (h <= dbb):
+        thickness = 0.2
+        h = h + 2*thickness
+        if ui:
+            ui.messageBox("Bearing's width too thin. Modified Thickness to: " + str(h))
+            
--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # 3D-printable-bearing-generator
-Generate 3D printable bearings according to your needs
+Generate 3D printable bearings according to your dimension constrains.
+
+<p align="middle">
+  <img src="/images/Generated Fusion 360 file.png" alt="Generated bearing" width=300>
+
+  <img src="/images/Generated Fusion 360 file section.png" alt="A section view of the generated bearing" width=300>
+
+</p>
+
+## 1. Introduction
+Many hobbyist face the problem of needing some bearings that are lightweight and cheap enough for their applications
+
+However, more times than not, the available sizes don´t match your application or they are to much of an overkill to be used.
+
+To solve these issues I decided to develop a Fusion 360 script which generates bearings depending on your input parameters.
+
+## 2. How to use
+Start by downloading and installing the script into your Fusion 360. Check [here](https://knowledge.autodesk.com/support/fusion-360/troubleshooting/caas/sfdcarticles/sfdcarticles/How-to-install-an-ADD-IN-and-Script-in-Fusion-360.html) for a quick guide.
+ 
+Once installed open the script and modify the  following bearing's parameters:
+
+<p align="middle">
+  <img src="/images/Bearing parameters.png" alt="The input parameters to generate the bearing" width=300>
+</p>
+
+The script will inform you if a value introduced by you doesn't make sense (i.e, outer diameter < inner diameter,...).
+
+## 3. Assembly
+
+For the assembly of the bearings follow next steps:
+1. 3D print bearing's two parts.
+2. Remove the support material
+3. Place a BB ball into the outer part's groove and turn the outer part looking for positions in which the ball gets stuck. Clean those positions until the ball is able to rotate freely.
+
+--- Insert picture ---
+
+4. Repeat previous step in the inner part's groove, by placing a BB on it and turning the inner part. Make sure that the BB ball slides smoothly.
+
+--- Insert picture ---
+
+5. Place the inner part inside the outer part and align the holes.
+
+--- Insert picture ---
+
+6. Start inserting BBs until there is no more room left. During the process make sure to turn the bearing to dectect any kind of blockage. If you encounter a blockage make sure the bea
+
+--- Insert picture ---
+
+7. Finish by inserting BBs until there is no more room left.
+7. Turn the bearing and see if it turn smoothly.
+
+
+## 4. Disassembly
+The diassmebly process could be a little bit troublesome depending on the tolerances you used.
+
+1. Align the insertion holes.
+2. With a thin tool align on BB ball in a way it is below the insertion hole.
+3. With another thin tool push the BB ball from behind though the extraction hole.
+4. Repeat 2-3 steps until there are no BB balls left.
+
+## 4. Software
+The script works in the following manner:
+
+1. Check the input parameters are correct
+2. Draw the inner and outer circles and extrude them (create new body operation).
+3. Draw the gap circle and extrude it (cut operation).
+4. Draw the BB ball circle and revolute it (cut operation).
+5. Draw the insertion and extraction circles and extrude them (cut operation).
+
+## 5. Requirements
+You should install/have the following:
+
+* Fusion 360
+* BB balls
+* 3D printer
+
+
+## Future functionalities
+
+In future versions I would like to add a GUI interface for inputting the parameter values. I would also like to include other bearing models with optional features (assembly holes,...).
+
